728c1691d57084d903e655911475c00171992878
Cicero Alcantara <cicero.alcantara17@gmail.com>
2019-08-23 15:23:27 -0300

    segundo commit

--- questao_02.py
+++ questao_02.py
@@ -1,40 +1,40 @@
 def main():
-    print("01 - Reserva de bilhete")
-    print("02 - cancelamento de bilhete")
-    print("03 - Verificacao de reserva")
-    print("04 - Exibicao de passageiros")
+    print("1 - Reserva de bilhete")
+    print("2 - Cancelamento de bilhete")
+    print("3 - Verificacao de reserva")
+    print("4 - Exibicao de passageiros")
 
     passageiros = []
 
-    opcao = int(input("Escolha uma opcao(01, 02, 03, 04)"))
+    opcao = int(input("Escolha uma opcao(1, 2, 3, 4)"))
 
-    for i in range(0, 15):
-        if opcao == 0o1:
+    while main != 4:
+        if opcao == 1:
             nome = str(input("Qual o nome do passageiro? "))
             passageiros.append(nome)
             print(passageiros)
-            opcao = int(input("Escolha uma opcao(01, 02, 03, 04)"))
+            opcao = int(input("Escolha uma opcao(1, 2, 3, 4)"))
 
-        elif opcao == 0o2:
+        elif opcao == 2:
             nome_apaga = str(input("Qual nome que deseja cancelar? "))
             passageiros.remove(nome_apaga)
             print(passageiros)
-            opcao = int(input("Escolha uma opcao(01, 02, 03, 04)"))
+            opcao = int(input("Escolha uma opcao(1, 2, 3, 4)"))
 
-        elif opcao == 0o3:
+        elif opcao == 3:
             nome_consulta = str(input("Qual nome deseja consultar? "))
             posicao = passageiros.index(nome_consulta)
-            print("O passageiro: ", nome_consulta, ", ja consta na lista, ", passageiros, ", na posicao: ", posicao + 1)
-            opcao = int(input("Escolha uma opcao(01, 02, 03, 04)"))
+            print("O passageiro: ", nome_consulta, ", consta na lista, ", passageiros, ", na posicao: ", posicao + 1)
+            opcao = int(input("Escolha uma opcao(1, 2, 3, 4)"))
 
-        elif opcao == 0o4:
+        elif opcao == 4:
             print("A lista de passageiros em ordem alfabetica: ")
             print(sorted(passageiros))
-            opcao = int(input("Escolha uma opcao(01, 02, 03, 04)"))
+            opcao = int(input("Escolha uma opcao(1, 2, 3, 4)"))
 
         else:
             print("Opcao errada!")
-            opcao = int(input("Escolha uma opcao(01, 02, 03, 04)"))
+            opcao = int(input("Escolha uma opcao(1, 2, 3, 4)"))
 
 
 main()
